8194a5b4928d910aa3d9c020e5b22edd67795f46
William Zajac <william.zajac@disney.com>
2018-01-11 11:42:18 -0800

    almost working

--- README.md
+++ README.md
@@ -4,13 +4,27 @@ Notes
 * https://mobius.network/store/developer
 * https://mobius.network/docs/?python#app-store
 
+* https://github.com/zulucrypto/mobius-dapp-quickstart/blob/master/src/AppBundle/Controller/CoinFlipController.php
+
 * https://mobius-marcus-aurelius.appspot.com
+    * https://mobius-marcus-aurelius.appspot.com/static/marcus-aurelius.png
 
 Install
 --------
 * https://cloud.google.com/appengine/docs/standard/python/tools/using-libraries-python-27
 ```
-pip install -t lib -r requirements.txt
+# pip2 install --upgrade pip setuptools
+pip2 install -t lib -r requirements.txt
+```
+
+Test
+--------
+```
+dev_appserver.py .
+```
+
+```
+curl "localhost:8080?email=test@testing.com&api_key=YOUR_API_KEY"
 ```
 
 Deploy
@@ -20,3 +34,11 @@ Deploy
 gcloud app deploy app.yaml
 ```
 
+Mobius Testing
+--------
+```
+curl -G "https://mobius.network/api/v1/app_store/balance" \
+     -H "x-api-key: API_KEY_HERE" \
+     -d "app_uid=APP_UID" \
+     -d "email=EMAIL"
+```
--- main.py
+++ main.py
@@ -15,12 +15,33 @@
 import webapp2
 import random
 import meditations
+import config
+from pymobius import Mobius
+import json
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(random.choice(meditations.quotes))
+        # Init the mobius object
+        mobius = Mobius(api_key=config.api_key)
 
+        # Get the user's email and the api-key
+        email = self.request.get_all("email")
+        request_api_key = self.request.get_all("api_key")
+
+        # Prep the response
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        # Validate the api-key
+        if (request_api_key == config.api_key):
+            self.response.write("Invalid API Key")
+
+        resp_json = mobius.app_store.use(app_uid=config.uid, email=email, num_credits=1)
+        resp = json.loads(resp_json)
+
+        if resp["success"]:
+            self.response.write(random.choice(meditations.quotes))
+        else:
+            self.response.write("Not enough tokens")
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
